cc8d8a83e7383c2acffe48dc922391c03f555f43
magillos <kerown@gmail.com>
2026-05-16 14:51:53 +0100

    added Fluthub remote in buid script

--- Flatpak/rebuild.sh
+++ Flatpak/rebuild.sh
@@ -1,5 +1,6 @@
 flatpak-builder --repo=repo --force-clean build-dir com.github.magillos.cable.yaml;
-flatpak build-bundle repo com.github.magillos.cable.flatpak com.github.magillos.cable;
+flatpak build-bundle --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo repo com.github.magillos.cable.flatpak com.github.magillos.cable;
+#flatpak build-bundle repo com.github.magillos.cable.flatpak com.github.magillos.cable;
 flatpak install --user com.github.magillos.cable.flatpak;
 flatpak run com.github.magillos.cable;
 
